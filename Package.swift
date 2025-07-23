d24c45c02e2dd36d0956d127b16d1a3ab8d0a802
siddharth <siddharth.k@mapmyindia.com>
2025-07-23 12:01:12 +0530

    change log:
    - Added a provision to restrict setting the default style.
    
    Released version: 6.0.1

--- Package.swift
+++ Package.swift
@@ -12,14 +12,14 @@ let package = Package(
             targets: ["MapplsMapWrapper"])
     ],
     dependencies: [
-        .package(url: "https://github.com/mappls-api/mappls-api-core-ios-distribution.git", from: "2.0.4"),
-        .package(url: "https://github.com/mappls-api/mappls-api-kit-ios-distribution.git", from: "3.0.0")
+        .package(url: "https://github.com/mappls-api/mappls-api-core-ios-distribution.git", from: "2.0.5"),
+        .package(url: "https://github.com/mappls-api/mappls-api-kit-ios-distribution.git", from: "3.0.1")
     ],
     targets: [
         .binaryTarget(
             name: "MapplsMap",
-            url: "https://mmi-api-team.s3.amazonaws.com/mappls-sdk-ios/mappls-map/MapplsMap.xcframework-6.0.0.zip",
-            checksum: "8c3f8c7d4270ab8d83f3a170e161422958d2a668dd168dcc48d572bf61be732f"
+            url: "https://mmi-api-team.s3.amazonaws.com/mappls-sdk-ios/mappls-map/MapplsMap.xcframework-6.0.1.zip",
+            checksum: "1a16dd71b34e8f8219c48b07048c3a8605af2a3bc947c78a3ef0863d3a54c3df"
         ),
         .target(
             name: "MapplsMapWrapper",
