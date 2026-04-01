28df95fcaaf07446c682fb1ca628c36eafa8dd94
siddharth <siddharth.k@mapmyindia.com>
2026-04-01 11:59:20 +0530

    Released version 6.1.3
    Change log:
    
    The carplay fullscreen map issue has been fixed. Additionally, a 3D building plugin has been added to hide and show 3D buildings.

--- Package.swift
+++ Package.swift
@@ -12,14 +12,14 @@ let package = Package(
             targets: ["MapplsMapWrapper"])
     ],
     dependencies: [
-        .package(url: "https://github.com/mappls-api/mappls-api-core-ios-distribution.git", from: "2.0.5"),
-        .package(url: "https://github.com/mappls-api/mappls-api-kit-ios-distribution.git", from: "3.0.1")
+        .package(url: "https://github.com/mappls-api/mappls-api-core-ios-distribution.git", from: "2.1.2"),
+        .package(url: "https://github.com/mappls-api/mappls-api-kit-ios-distribution.git", from: "3.0.4")
     ],
     targets: [
         .binaryTarget(
             name: "MapplsMap",
-            url: "https://mmi-api-team.s3.amazonaws.com/mappls-sdk-ios/mappls-map/MapplsMap.xcframework-6.0.1.zip",
-            checksum: "1a16dd71b34e8f8219c48b07048c3a8605af2a3bc947c78a3ef0863d3a54c3df"
+            url: "https://mmi-api-team.s3.amazonaws.com/mappls-sdk-ios/mappls-map/MapplsMap.xcframework-6.1.3.zip",
+            checksum: "f93ad4b46e2e46cac2f8c397409802fb2b9dd513ea1f25915db0c138b2705142"
         ),
         .target(
             name: "MapplsMapWrapper",
