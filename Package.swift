51a5b77c788595b272afb9a4e83a4283c36f4f04
siddharth <siddharth.k@mapmyindia.com>
2026-07-02 17:32:34 +0530

    Released 6.1.5
    - Fixed an issue where regional language map labels were displayed on a single line.
    - Fixed logo placement issue in ios 18

--- Package.swift
+++ Package.swift
@@ -12,14 +12,14 @@ let package = Package(
             targets: ["MapplsMapWrapper"])
     ],
     dependencies: [
-        .package(url: "https://github.com/mappls-api/mappls-api-core-ios-distribution.git", from: "2.1.2"),
-        .package(url: "https://github.com/mappls-api/mappls-api-kit-ios-distribution.git", from: "3.0.4")
+        .package(url: "https://github.com/mappls-api/mappls-api-core-ios-distribution.git", from: "2.1.3"),
+        .package(url: "https://github.com/mappls-api/mappls-api-kit-ios-distribution.git", from: "3.0.6")
     ],
     targets: [
         .binaryTarget(
             name: "MapplsMap",
-            url: "https://mmi-api-team.s3.amazonaws.com/mappls-sdk-ios/mappls-map/MapplsMap.xcframework-6.1.4.zip",
-            checksum: "239f826001581e141eb44fc6bc572b3a39a0c9562cb12ac6936da69ac6c8391b"
+            url: "https://mmi-api-team.s3.amazonaws.com/mappls-sdk-ios/mappls-map/MapplsMap.xcframework-6.1.5.zip",
+            checksum: "fbc4e5edd02330482ea34ffb27c045347846f5b28ff8b648e1d46df9732b8646"
         ),
         .target(
             name: "MapplsMapWrapper",
